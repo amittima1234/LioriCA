54a4d6a6133822c47ef365e7dbd6e00edb744e5c
Amit Mendler <amittima1234@gmail.com>
2022-07-24 22:01:26 +0300

    fix(app.js): adding missing app.js file from last commit

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const { execSync } = require("node:child_process");
 const app = express();
 const port = 8008;
+
+const CA_FOLDER_PATH = "/home/amit/ca";
+const BACKEND_PATH = "/home/amit/git-projects/LioriCA/backend";
+const CERT_TYPE = "server";
+
+app.use(express.json());
 
 app.use(
   fileUpload({
@@ -33,9 +40,32 @@ app.post("/uploadReq", (req, res) => {
   }
 });
 
-app.post("/submitReq", (req, res) => {
+app.post("/submitReq", async (req, res) => {
+  console.log(req.body);
   const reqName = req.body.reqMD5;
-})
+  execSync(
+    `./scripts/importAndSubmitReq.sh ${CA_FOLDER_PATH} ${BACKEND_PATH}/reqs/${reqName}.req ${reqName} ${CERT_TYPE}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+  const fileName = `${CA_FOLDER_PATH}/pki/issued/${reqName}.crt`
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
