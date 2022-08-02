c02a4ec479eb7649fcd53e0544f523074b3b914f
Amit Mendler <amittima1234@gmail.com>
2022-08-02 22:14:15 +0300

    Added navbar buttons

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,11 +1,22 @@
 import styles from "./index.module.css";
 import NavbarButton from "../navbarButton";
+import { IoLogIn } from "react-icons/io5";
+import { TbFileCertificate } from "react-icons/tb";
+import { BsFileEarmarkMedicalFill } from "react-icons/bs";
 
-export default function Navbar () {
+
+
+export default function Navbar() {
   return (
-    <div className={styles.navbar} >
-      <NavbarButton title={"Login"} to={"/login"} />
-      <NavbarButton title={"Request a certificate"} to={"/uploadReqForm"} />
+    <div className={styles.container}>
+      <div className={styles["buttons-container"]}>
+        <NavbarButton title={"Login"} to={"/login"} Icon={IoLogIn} />
+        <NavbarButton
+          title={"Request a Certificate"}
+          to={"/uploadReqForm"}
+          Icon={BsFileEarmarkMedicalFill}
+        />
+      </div>
     </div>
   );
-};
+}
--- frontend/src/components/navbarButton/index.js
+++ frontend/src/components/navbarButton/index.js
@@ -1,10 +1,15 @@
 import styles from "./index.module.css";
 import { Link } from "react-router-dom";
 
-export default function NavbarButton({ title, to }) {
+export default function NavbarButton({ title, to, Icon }) {
   return (
-    <Link className={styles["navnabr-button"]} to={to}>
-      {title}
+    <Link className={styles.container} to={to}>
+      <span className={styles.icon}>
+        <Icon />
+      </span>
+      <div className={styles.text}>
+        <span className={styles.text}>{title}</span>
+      </div>
     </Link>
   );
 }
