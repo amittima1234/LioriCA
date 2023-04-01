8ddfcd07e6c52c30d4341af3fd52f83d447b7b2c
unknown <kakithebaki@gamil.com>
2023-04-01 22:03:35 +0300

    Add HomePage component

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../pages/login';
-import Uploud from './upload';
-import Navbar from './navbar';
-import './app.css';
+import './App.css';
+import HomePage from './HomePage/HomePage';
 
 export default function App() {
   return (
-    <div className="app">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/upload" element={<Uploud />} />
-        </Routes>
-      </Router>
-    </div>
+    <HomePage></HomePage>
   );
 }
