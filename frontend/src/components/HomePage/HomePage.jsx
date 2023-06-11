ae94e9b964aed25e8d5b17c3f8b2a50b94ebba06
unknown <kakithebaki@gamil.com>
2023-06-11 22:42:16 +0300

    navigate to Login component on button click

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './HomePage.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    navigate('/login');
+  };
+
   return (
     <header className="header">
-        <div className="text-box">
-            <h1 className="heading-primary">
-                <span className="heading-primary-main">Liori-CA</span>
-                <span className="heading-primary-sub">פה מקבלים תעודות</span>
-            </h1>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Liori-CA</span>
+          <span className="heading-primary-sub">פה מקבלים תעודות</span>
+        </h1>
 
-            <a href="#" className="btn btn-white btn-animated">התחברות</a>
-        </div>
+        <button className="btn btn-white btn-animated" onClick={onButtonClick}>
+          התחברות
+        </button>
+      </div>
     </header>
   );
 }
--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './HomePage/HomePage';
+import Login from './Login/Login';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
