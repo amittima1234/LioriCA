22ff91543c3b6b5af1afbc6653a59e05406021df
Gal Naor <galnaor86@gmail.com>
2024-01-14 15:56:16 +0200

    add home icon on certificates pages

--- frontend/src/Components/AllCertificates/AllCertificates.tsx
+++ frontend/src/Components/AllCertificates/AllCertificates.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import homeIcon from '../../Assets/Images/home.png';
 import Certificate from '../../Models/Certificate';
 import certificatesService from '../../Services/CertificatesService';
 import CertificateCard from '../CertificateCard/CertificateCard';
@@ -6,6 +8,7 @@ import './AllCertificates.css';
 
 function AllCertificates(): JSX.Element {
     const [certificates, setCertificates] = useState<Certificate[]>([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         certificatesService
@@ -14,18 +17,27 @@ function AllCertificates(): JSX.Element {
             .catch((err) => console.error(err.message));
     }, []);
 
+    const navigateHome = () => {
+        navigate('/home');
+    };
+
     return (
-        <div className="Certificates">
-            <h1>כל התעודות</h1>
-            <div>
-                {certificates.map((certificate) => (
-                    <CertificateCard
-                        key={certificate._id}
-                        certificate={certificate}
-                    />
-                ))}
+        <>
+            <div className="home">
+                <img src={homeIcon} width={30} onClick={navigateHome}></img>
             </div>
-        </div>
+            <div className="Certificates">
+                <h1>כל התעודות</h1>
+                <div>
+                    {certificates.map((certificate) => (
+                        <CertificateCard
+                            key={certificate._id}
+                            certificate={certificate}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 
--- frontend/src/Components/Certificates/Certificates.tsx
+++ frontend/src/Components/Certificates/Certificates.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import homeIcon from '../../Assets/Images/home.png';
 import { AppState } from '../../Redux/AppState';
 import certificatesService from '../../Services/CertificatesService';
 import CertificateCard from '../CertificateCard/CertificateCard';
@@ -11,6 +12,7 @@ function Certificates(): JSX.Element {
         (appState: AppState) => appState.certificates
     ); // certifiactes subscription
     const _id = useSelector((appState: AppState) => appState.user._id);
+    const navigate = useNavigate();
 
     useEffect(() => {
         certificatesService
@@ -18,22 +20,33 @@ function Certificates(): JSX.Element {
             .catch((err) => console.error(err.message));
     }, []);
 
-    return (
-        <div className="Certificates">
-            <h1>התעודות שלי</h1>
-            <div>
-                {certificates.map((certificate) => (
-                    <CertificateCard
-                        key={certificate._id}
-                        certificate={certificate}
-                    />
-                ))}
-            </div>
+    const navigateHome = () => {
+        navigate('/home');
+    };
 
-            <NavLink to="/certificates/new">
-                <button className="NewCertificateBtn">הנפקת תעודה חדשה</button>
-            </NavLink>
-        </div>
+    return (
+        <>
+            <div className="home">
+                <img src={homeIcon} width={30} onClick={navigateHome}></img>
+            </div>
+            <div className="Certificates">
+                <h1>התעודות שלי</h1>
+                <div>
+                    {certificates.map((certificate) => (
+                        <CertificateCard
+                            key={certificate._id}
+                            certificate={certificate}
+                        />
+                    ))}
+                </div>
+
+                <NavLink to="/certificates/new">
+                    <button className="NewCertificateBtn">
+                        הנפקת תעודה חדשה
+                    </button>
+                </NavLink>
+            </div>
+        </>
     );
 }
 
